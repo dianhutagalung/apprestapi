f5eb3395216266982366252a60ea432fbb95bcd8
Dian_P <pdian096@gmail.com>
2020-12-06 09:32:04 +0700

    Berhasil menampilkan data mahasiswa

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ const response = require('./res');
 let connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("Aplikasi REST API ku berjalan!")
+    response.ok("Aplikasi REST API ku berjalan!",res)
+}
+
+//menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req,res){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields){
+        if(error){
+            connection.log(error)
+        }else{
+            response.ok(rows, res)
+        }
+    })
 }
--- routes.js
+++ routes.js
@@ -4,5 +4,8 @@ module.exports = function(app){
     let jsonku = require('./controller');
 
     app.route('/')
-    .get(jsonku.index)
+    .get(jsonku.index);
+
+    app.route('/mhs')
+    .get(jsonku.tampilsemuamahasiswa)
 }
